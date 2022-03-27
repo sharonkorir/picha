9ed494be3113061f8ab731b431e3ccec9624e9b2
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-27 13:50:49 +0300

    create home page template

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,12 @@
+from cgitb import html
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def test(request):
     return HttpResponse('testing gallery')
+
+def show_images(request):
+    
+    return render(request, 'index.html')
--- picha/settings.py
+++ picha/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
