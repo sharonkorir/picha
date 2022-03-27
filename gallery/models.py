9136198f672289cc89a4c85fcb15b74cd3427c76
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-27 15:42:37 +0300

    database: create location model

--- gallery/models.py
+++ gallery/models.py
@@ -5,3 +5,12 @@ class Owner(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     #email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+class location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
